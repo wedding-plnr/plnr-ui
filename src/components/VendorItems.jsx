667e0e57d86a47ed33815af93120186218db1c3f
clay g <claytonbreazeale@yahoo.com>
2023-04-13 13:36:16 -0500

    added ability to delete vendors

--- src/components/VendorItems.jsx
+++ src/components/VendorItems.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
+// import FlipMove from 'react-flip-move';
 
 class VendorItems extends Component {
+  constructor(props) {
+    super(props);
+    this.createTasks = this.createTasks.bind(this);
+  }
+
+  delete(key) {
+    this.props.delete(key);
+  }
+
   createTasks(item) {
-    return <li key={item.key}>{item.text}</li>;
+    return (
+      <li onClick={() => this.delete(item.key)} key={item.key}>
+        {item.text}
+      </li>
+    );
   }
 
   render() {
-    const vendorEntries = this.props.entries;
-    const listItems = vendorEntries.map(this.createTasks);
+    var vendorEntries = this.props.entries;
+    var listItems = vendorEntries.map(this.createTasks);
 
     return <ul className="theList">{listItems}</ul>;
   }
--- src/components/VendorList.jsx
+++ src/components/VendorList.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-// import { Form } from "formik";
-import "./css/VendorList.css";
 import VendorItems from "./VendorItems";
+import './css/VendorsList.css'
 
 class VendorList extends Component {
   constructor(props) {
@@ -12,42 +11,49 @@ class VendorList extends Component {
     };
 
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
 
   addItem(e) {
+    var itemArray = this.state.items;
+
     if (this._inputElement.value !== "") {
-      let newItem = {
+      itemArray.unshift({
         text: this._inputElement.value,
         key: Date.now(),
-      };
+      });
 
-      this.setState((prevState) => {
-        return {
-          items: prevState.items.concat(newItem),
-        };
+      this.setState({
+        items: itemArray,
       });
 
       this._inputElement.value = "";
     }
-
-    console.log(this.state.items);
-
+    console.log(itemArray);
     e.preventDefault();
+  }
+
+  deleteItem(key) {
+    var filteredItems = this.state.items.filter(function (item) {
+      return item.key !== key;
+    });
+
+    this.setState({
+      items: filteredItems,
+    });
   }
 
   render() {
     return (
       <div className="vendorListMain">
-        <div className="header">
-          <form className="vendorList" onSubmit={this.addItem}>
+          <form onSubmit={this.addItem}>
             <input
               ref={(a) => (this._inputElement = a)}
-              placeholder="enter vendor"
+              placeholder="enter task"
             ></input>
             <button type="submit">add</button>
           </form>
-        <VendorItems entries={this.state.items} />
-        </div>
+        <VendorItems entries={this.state.items} delete={this.deleteItem} />
       </div>
     );
   }
